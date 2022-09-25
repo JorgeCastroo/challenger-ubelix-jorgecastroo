8448efec7a41f0fb214401f779d2cd229c7af8bb
jorgecastro <jorgeeduardo27007@gmail.com>
2022-09-25 20:34:46 -0300

    common changes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { AppRoutes } from "./src/routes/app.routes";
+import { ThemeProvider } from "styled-components";
 
-export default function App() {
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold,
+  Lato_900Black,
+} from "@expo-google-fonts/lato";
+import theme from "./src/styles/theme";
+
+const ApplicationContent = () => {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Lato_900Black,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {fontsLoaded && (
+        <>
+          <AppRoutes />
+        </>
+      )}
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <ApplicationContent />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
