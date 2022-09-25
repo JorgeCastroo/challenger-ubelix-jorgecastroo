2f2f2df4aa83325d099ef3526db0e02e611baacd
jorgecastro <jorgeeduardo27007@gmail.com>
2022-09-25 20:28:26 -0300

    feat: config styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    title_text: "black",
+    background: "#FFFFFF",
+    placeholder: "#828282",
+    gray_one: "#333333",
+    gray_two: "#4F4F4F",
+    white: "#FFFFFF",
+    flag_background: "#D40000",
+  },
+  fonts: {
+    lato_regular: "Lato_400Regular",
+    lato_bold: "Lato_700Bold",
+    lato_black: "Lato_900Black",
+  },
+};
