2c4e1ed38523211faaddfdd0eeb4038b3fa4291e
jorgecastro <jorgeeduardo27007@gmail.com>
2022-09-25 21:16:44 -0300

    release: finish app

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,8 +6,6 @@ export default {
     gray_one: "#333333",
     gray_two: "#4F4F4F",
     white: "#FFFFFF",
-    flag_background: "#D40000",
-  },
   fonts: {
     lato_regular: "Lato_400Regular",
     lato_bold: "Lato_700Bold",
